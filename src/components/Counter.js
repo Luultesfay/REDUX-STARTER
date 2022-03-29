29317fdd479c7b2f9e8f5b894581779890339dfc
luul negasi <luultesfay24@gmail.com>
2022-03-28 17:28:08 -0700

    redux toolkit added and configureStore() ,createSlice() added

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,33 +1,38 @@
+import { useSelector, useDispatch } from "react-redux";
+
+import { counterActions } from "../store/indexStore";
 import classes from "./Counter.module.css";
-import { useSelector, useDispatch } from "react-redux"; //useSelector is a custom hook made by the React Redux team. that helps as to select part of the state from the store
 
 const Counter = () => {
-  //we pass a function to the useSelector Hook  and that function executed by react-redux
-  const counter = useSelector((state) => state.counter); // function which then basically determines which piece of data we wanna extract from our store.
-  const Show = useSelector((state) => state.ShowCounter);
-  const dispatch = useDispatch(); // useDispatch hook is povided by react-redux  and that hook  give as a 'dispatch function'  to despatch actions to the reducer function ,
-  const toggleCounterHandler = () => {
-    dispatch({ type: "toggle" });
+  const dispatch = useDispatch();
+  const counter = useSelector((state) => state.counter);
+  const show = useSelector((state) => state.showCounter);
+
+  const incrementHandler = () => {
+    //So we get a full action object automatically created,for us here.
+    dispatch(counterActions.increment()); //{ type: SOME_UNIQUE_IDENTIFIER }
   };
-  const IncrementHandler = () => {
-    dispatch({ type: "increment" }); //we despatch actions to reducerFn in the store
-  };
-  const decrementHandler = () => {
-    dispatch({ type: "decerement" });
-  };
+  //So we get a full action object automatically created,for us here.
   const increaseHandler = () => {
-    dispatch({ type: "increase", amount: 10 }); //we can also add another payload  called amount   this amount should be the same amount with the action.amount in the reducer function
+    dispatch(counterActions.increase(10)); // { type: SOME_UNIQUE_IDENTIFIER, payload: 10 }//payload is default name given by toolkit
+  };
+
+  const decrementHandler = () => {
+    dispatch(counterActions.decrement());
+  };
+
+  const toggleCounterHandler = () => {
+    dispatch(counterActions.toggleCounter());
   };
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      {/* <div className={classes.value}>-- COUNTER VALUE --</div> */}
-      {Show && <div className={classes.value}>{counter}</div>}
+      {show && <div className={classes.value}>{counter}</div>}
       <div>
-        <button onClick={IncrementHandler}>increment</button>
-        <button onClick={increaseHandler}>increase</button>
-        <button onClick={decrementHandler}>decrement</button>
+        <button onClick={incrementHandler}>Increment</button>
+        <button onClick={increaseHandler}>Increase by 10</button>
+        <button onClick={decrementHandler}>Decrement</button>
       </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
@@ -35,6 +40,50 @@ const Counter = () => {
 };
 
 export default Counter;
+// import classes from "./Counter.module.css";
+// import { useSelector, useDispatch } from "react-redux"; //useSelector is a custom hook made by the React Redux team. that helps as to select part of the state from the store
+// import { counterAction } from "../store/indexStore";
+
+// const Counter = () => {
+//   const dispatch = useDispatch(); // useDispatch hook is povided by react-redux  and that hook  give as a 'dispatch function'  to despatch actions to the reducer function ,
+//   //we pass a function to the useSelector Hook  and that function executed by react-redux
+//   const counter = useSelector((state) => state.counter); // function which then basically determines which piece of data we wanna extract from our store.
+//   const Show = useSelector((state) => state.ShowCounter);
+
+//   const IncrementHandler = () => {
+//     //dispatch({ type: "increment" }); //we despatch actions to reducerFn in the store
+//     dispatch(counterAction.increment());
+//   };
+//   const decrementHandler = () => {
+//     //dispatch({ type: "decerement" });
+//     dispatch(counterAction.decerement());
+//   };
+//   const increaseHandler = () => {
+//     //dispatch({ type: "increase", amount: 10 }); //we can also add another payload  called amount   this amount should be the same amount with the action.amount in the reducer function
+//     dispatch(counterAction.increase(10));
+//   };
+//   const toggleCounterHandler = () => {
+//     //dispatch({ type: "toggle" }); is replaced by  method created from createSlice
+//     dispatch(counterAction.toggle()); //dispatch action
+//   };
+
+//   return (
+//     <main className={classes.counter}>
+//       <h1>Redux Counter</h1>
+//       {/* <div className={classes.value}>-- COUNTER VALUE --</div> */}
+//       {Show && <div className={classes.value}>{counter}</div>}
+//       <div>
+//         <button onClick={IncrementHandler}>increment</button>
+//         <button onClick={increaseHandler}>increase</button>
+//         <button onClick={decrementHandler}>decrement</button>
+//       </div>
+//       <button onClick={toggleCounterHandler}>Toggle Counter</button>
+//     </main>
+//   );
+// };
+
+// export default Counter;
+
 /*
 ///NOTE:Now the great thing is that when you use 'useSelector',React Redux will automatically set up a subscription
 
--- src/store/indexStore.js
+++ src/store/indexStore.js
@@ -1,47 +1,128 @@
-import { createStore } from "redux";
-const initalState = { counter: 0, ShowCounter: true };
-const counterReducer = (state = initalState, action) => {
-  if (action.type === "increment") {
-    //NOTE:when working with Redux.You should never change the existing state.Instead, always override it by returning a brand new state object.
-    /*
-    //this is wrong   dont mutate existing state like this instead override it by returning it
-    
-    state ++
-    return state 
-    
-    ///or
-    state ++
-    return{
-      counter:state.counter
-      ShowCounter: state.ShowCounter,
-    } */
-    //correct way of overriding the existing state  by returning new brand object
-    //note when ever we return we should return exsting state with the modified one   for example here we added in all show counter
-    return {
-      counter: state.counter + 1,
-      ShowCounter: state.ShowCounter,
-    };
-  }
-  if (action.type === "increase") {
-    return {
-      counter: state.counter + action.amount,
-      ShowCounter: state.ShowCounter,
-    };
-  }
-  if (action.type === "decerement") {
-    return {
-      counter: state.counter - 1,
-      ShowCounter: state.ShowCounter,
-    };
-  }
-  if (action.type === "toggle") {
-    return {
-      counter: state.counter,
-      ShowCounter: !state.ShowCounter,
-    };
-  }
-  return state;
-};
-const store = createStore(counterReducer); //we create store and passed the reducer function on it
+///reduxtoolkit advantages
+/*-don't have to worry about creating action objects on our own
+and about coming up with unique identifiers
+and about avoiding typos*/
+import { createSlice, configureStore } from "@reduxjs/toolkit";
 
-export default store; // export the store and provide to the components
+const initialState = { counter: 0, showCounter: true };
+
+const counterSlice = createSlice({
+  name: "counter",
+  initialState,
+  reducers: {
+    increment(state) {
+      state.counter++;
+    },
+    decrement(state) {
+      state.counter--;
+    },
+    increase(state, action) {
+      state.counter = state.counter + action.payload; //payload is afield name That Redux Toolkit give us by default its not up to us to call it payload
+    },
+    toggleCounter(state) {
+      state.showCounter = !state.showCounter;
+    },
+  },
+});
+
+const store = configureStore({
+  reducer: counterSlice.reducer,
+});
+
+export const counterActions = counterSlice.actions; //counterSlice.actions.toggleCounter or increment or decrement  when this methods calls they produce action of objects with type property
+
+export default store;
+
+/*Note
+Redux Toolkit has a configureStore() method that simplifies the store setup process. 
+configureStore() wraps around the Redux library's createStore() method and the combineReducers() method, 
+and handles most of the store setup for us automatically.
+*/
+
+/////////////////////////////////////////////////////////////////////////////////
+// import { createSlice, configureStore } from "@reduxjs/toolkit";
+// const initalState = { counter: 0, ShowCounter: true };
+// /*
+//createSlice
+// A function that accepts an initial state, an object of reducer functions,
+// and a "slice name", and automatically generates action creators and action
+// types that correspond to the reducers and state.
+// */
+// const counterSlice = createSlice({
+//   name: "counter",
+//   initalState,
+//   reducers: {
+//     increment(state) {
+//       //here we passes the existing state  and then mutate the state , note: we  mutate the state wihch is wrong but redux toolkit fixes tis behind the scine using  'immer library'
+//       state.counter++; // we can mutate state since this is fixed by immer internally
+//     },
+
+//     decrement(state) {
+//       state.counter--;
+//     },
+//     increase(state, action) {
+//       state.counter = state.counter + action.payload; //amount changes with payload
+//     },
+//     toggleCounter(state) {
+//       state.ShowCounter = !state.ShowCounter;
+//     },
+//   },
+// });
+
+// /*configureStore(): wraps createStore to provide simplified configuration options and good defaults.
+//  It can automatically combine your slice reducers, adds whatever Redux middleware you supply,
+//  includes redux-thunk by default, and enables use of the Redux DevTools Extension.*/
+
+// ///ConfigureStore like createStore creates a store but it makes merging multiple reducers into one reducer
+
+// const store = configureStore({ reducer: counterSlice.reducer }); //we create store and passed the reducer function and now we r can access to reducer
+// export const counterActions = counterSlice.actions;
+// export default store; // export the store and provide to the components
+
+//////the below code replaced by the above code which is the same but shorter and managable
+//import { createStore } from "redux";//  we can  delete this one since we have this redux includede in @reduxjs/toolkit
+// const counterReducer = (state = initalState, action) => {
+//   if (action.type === "increment") {
+//     //NOTE:when working with Redux.You should never change the existing state.Instead, always override it by returning a brand new state object.
+//     /*
+//     //this is wrong   dont mutate existing state like this instead override it by returning it
+
+//     state ++
+//     return state
+
+//     ///or
+//     state ++
+//     return{
+//       counter:state.counter
+//       ShowCounter: state.ShowCounter,
+//     } */
+//     //correct way of overriding the existing state  by returning new brand object
+//     //note when ever we return we should return exsting state with the modified one   for example here we added in all show counter
+//     return {
+//       counter: state.counter + 1,
+//       ShowCounter: state.ShowCounter,
+//     };
+//   }
+//   if (action.type === "increase") {
+//     return {
+//       counter: state.counter + action.amount,
+//       ShowCounter: state.ShowCounter,
+//     };
+//   }
+//   if (action.type === "decerement") {
+//     return {
+//       counter: state.counter - 1,
+//       ShowCounter: state.ShowCounter,
+//     };
+//   }
+//   if (action.type === "toggle") {
+//     return {
+//       counter: state.counter,
+//       ShowCounter: !state.ShowCounter,
+//     };
+//   }
+//   return state;
+// };
+// const store = createStore(counterReducer); //we create store and passed the reducer function on it
+
+// export default store; // export the store and provide to the components
